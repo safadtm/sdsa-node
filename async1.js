88d20620a1b6bb3c70c14733106b4948c90b3320
safadtm <safadtm2004@gmail.com>
2024-11-21 06:27:45 +0530

    async method:

---
+++ async1.js
@@ -0,0 +1,5 @@
+console.log("Iam First");
+setTimeout(function timeout(){
+    console.log("Iam Second");
+},5000);
+console.log("Iam Third");
---
+++ async2.js
@@ -0,0 +1,12 @@
+var fs=require('fs');
+const { setTimeout } = require('timers');
+fs.readFile('test.txt',function(err,data){
+    if(err){
+        console.log(err);
+    }
+    setTimeout(()=>{
+        console.log("Display after 5 second");
+        console.log(data.toString());
+    },5000);
+});
+console.log("Start here")
